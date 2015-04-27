d3ab81411d91a5133a3ff6e6e35c9dba1eaccec4
Emma Johansson <emma@schenkman.info>
2015-04-27 13:48:34 +0200

    Initial Commit

---
+++ MoveItApp/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MoveItApp
+//
+//  Created by Emmiz on 2015-04-27.
+//  Copyright (c) 2015 EmmaJohansson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
